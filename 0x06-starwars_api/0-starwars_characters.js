5dec543bba21058f1464287fcdbff36e206370be
Geexix <gift85210@gmail.com>
2024-09-07 22:50:48 +0100

    starwar character

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,16 +7,28 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(url, async (err, res, body) => {
-  err && console.log(err);
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-  const charactersArray = (JSON.parse(res.body).characters);
+  // Parse the body to get the movie details
+  const movieData = JSON.parse(body);
+  const charactersArray = movieData.characters;
+
+  // Loop through each character URL in the charactersArray
   for (const character of charactersArray) {
     await new Promise((resolve, reject) => {
+      // Make a request to get the character details
       request(character, (err, res, body) => {
-        err && console.log(err);
-
-        console.log(JSON.parse(body).name);
-        resolve();
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          // Print the character name
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
       });
     });
   }
