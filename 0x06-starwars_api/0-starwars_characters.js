704b7894c628f2d49a6b863e9d5d44fdc1dad758
Geexix <gift85210@gmail.com>
2024-09-07 22:32:00 +0100

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(url, async (err, res, body) => {
   err && console.log(err);
